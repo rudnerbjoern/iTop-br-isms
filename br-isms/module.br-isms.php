64a5634d7aeb54827831a91b4ef973884f0d02e6
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-10-30 11:58:11 +0100

    prepare compatibility to iTop 3.1

--- br-isms/module.br-isms.php
+++ br-isms/module.br-isms.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-10-29
+ * @version     2024-10-30
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-isms/0.0.2',
+    'br-isms/3.1.2',
     array(
         // Identification
         //
@@ -20,8 +20,8 @@ SetupWebPage::AddModule(
         // Setup
         //
         'dependencies' => array(
-            '(itop-config-mgmt/2.5.0 & itop-config-mgmt/<3.0.0)||itop-structure/3.0.0',
-            'br-riskassessment/2.7.8',
+            'itop-config-mgmt/3.1.0',
+            'br-riskassessment/3.1.8',
         ),
         'mandatory' => false,
         'visible' => true,
@@ -29,9 +29,7 @@ SetupWebPage::AddModule(
 
         // Components
         //
-        'datamodel' => array(
-            'model.br-isms.php'
-        ),
+        'datamodel' => array(),
         'webservice' => array(),
         'data.struct' => array(
             // add your 'structure' definition XML files here,
