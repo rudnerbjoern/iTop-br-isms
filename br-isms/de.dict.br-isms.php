fc6f2ea092897c33b552a5e524636872515d5d15
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-10-30 12:58:13 +0100

    Update Dictionary Items

--- br-isms/de.dict.br-isms.php
+++ br-isms/de.dict.br-isms.php
@@ -4,6 +4,7 @@
  * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
  * @version     2024-10-30
+ *
  * Localized data
  */
 
@@ -20,6 +21,7 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
 // Class: ISMSAsset
 //
 Dict::Add('DE DE', 'German', 'Deutsch', array(
+    'ISMSAsset:Contacts' => 'Kontakte',
     'Class:ISMSAsset' => 'Asset',
     'Class:ISMSAsset/Attribute:name' => 'Name',
     'Class:ISMSAsset/Attribute:organization_name' => 'Organisation',
@@ -31,6 +33,21 @@ Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:ISMSAsset/Attribute:category/Value:primary' => 'Primär',
     'Class:ISMSAsset/Attribute:category/Value:secondary' => 'Sekundär',
     'Class:ISMSAsset/Attribute:description' => 'Beschreibung',
+    'Class:ISMSAsset/Attribute:assettype_id' => 'Typ',
+    'Class:ISMSAsset/Attribute:assettype_name' => 'Typ Name',
+    'Class:ISMSAsset/Attribute:assetowner_id' => 'Eigentümer',
+    'Class:ISMSAsset/Attribute:assetowner_id+' => 'Der Name des Asset Eigentümers.',
+    'Class:ISMSAsset/Attribute:assetowner_name' => 'Eigentümer Name',
+    'Class:ISMSAsset/Attribute:assetguardian_id' => 'Betreiber',
+    'Class:ISMSAsset/Attribute:assetguardian_id+' => 'Der Name des Betreibers des Assets.',
+    'Class:ISMSAsset/Attribute:assetguardian_name' => 'Betreiber Name',
+    'Class:ISMSAsset/Attribute:assetusers' => 'Anwender',
+    'Class:ISMSAsset/Attribute:assetusers+' => 'Informationen über die Anwender dieses Assets.',
+    'Class:ISMSAsset/Attribute:creation_date' => 'Erstelldatum',
+    'Class:ISMSAsset/Attribute:last_update' => 'Letzte Änderung',
+    'Class:ISMSAsset/Attribute:next_revision' => 'Nächste Revision',
+    'Class:ISMSAsset/Attribute:supportingassets_list' => 'Unterstützende Assets',
+    'Class:ISMSAsset/Attribute:supportedassets_list' => 'Unterstützte Assets',
     'Class:ISMSAssetType' => 'Asset Typ',
     'Class:ISMSAssetType/Attribute:name' => 'Name',
     'Class:ISMSAssetType/Attribute:description' => 'Beschreibung',
