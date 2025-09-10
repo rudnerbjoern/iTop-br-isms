9a03a3902a5dac06dcbced46cfe249ba755de915
Björn Rudner <rudnerbjoern@hotmail.com>
2025-09-10 18:28:32 +0200

    set version 3.2.1

--- br-isms/module.br-isms.php
+++ br-isms/module.br-isms.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (C) 2024-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/agpl-3.0.en.html
- * @version     2025-09-09
+ * @version     2025-09-10
  *
  * iTop module definition file
  */
@@ -11,7 +11,7 @@
 /** @disregard P1009 Undefined type SetupWebPage */
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-isms/3.2.0',
+    'br-isms/3.2.1',
     array(
         // Identification
         //
